4fc7f7c9143484cdcfe32011c81f7424ad382815
Rodrigo Mello <rodbove@gmail.com>
2021-07-03 20:18:13 -0300

    test: add success search and empty string search test cases

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
+use std::io::Write;
 use std::process::Command; // Run programs
+use tempfile::NamedTempFile; // Creates temp file
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,6 +13,36 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     .assert()
     .failure()
     .stderr(predicate::str::contains("No such file or directory"));
+
+  Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+  let mut file = NamedTempFile::new()?;
+  writeln!(file, "Temporary test file\nlorem ipsum\ntest ipsum\nloremus")?;
+
+  let mut cmd = Command::cargo_bin("grrs")?;
+
+  cmd.arg("lorem").arg(file.path());
+  cmd.assert()
+    .success()
+    .stdout(predicate::str::contains("lorem ipsum\nloremus"));
+
+  Ok(())
+}
+
+#[test]
+fn find_empty_pattern() -> Result<(), Box<dyn std::error::Error>> {
+  let mut file = NamedTempFile::new()?;
+  writeln!(file, "Temporary test file\nlorem ipsum\ntest ipsum\nloremus")?;
+
+  let mut cmd = Command::cargo_bin("grrs")?;
+
+  cmd.arg("").arg(file.path());
+  cmd.assert()
+    .success()
+    .stdout(predicate::str::contains("Temporary test file\nlorem ipsum\ntest ipsum\nloremus"));
 
   Ok(())
 }
